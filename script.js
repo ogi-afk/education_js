46f3c93279d91f0b705772d8b355eab1f460c700
ogi-afk <r.arechwa@gmail.com>
2022-06-08 18:41:41 +0200

    compleat practicle_19team

---
+++ script.js
@@ -0,0 +1,22 @@
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", '');
+const personalMovieDB:{
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const lastFilm = prompt ('Один из последних просмотренных фильмов',''),
+      scoreFilm = prompt('На сколько оцените фильм', ''),
+      lastFilm2 = prompt ('Один из последних просмотренных фильмов', ""),
+      scoreFilm2 = prompt('На сколько оцените фильм', '');
+
+const movies: {
+  'logan':'8.1'
+}
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+
+console.log(personalMovieDB)
